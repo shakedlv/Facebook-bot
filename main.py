5f9c142487eaa13052bdee394868eac3e1453e42
Shaked Levy <88069759+shakedlv@users.noreply.github.com>
2022-01-08 19:56:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from  selenium.webdriver.common.by import  By
+from time import  sleep
+import datetime
+import random
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
+
+driver.get("https://www.facebook.com")
+
+
+_input = input("Press enter after login")
+
+driver.get("https://www.facebook.com/189917291362525/posts/1608121936208713/")
+
+_input = input("Press enter after replay box clicked")
+
+box = "//*[@aria-label='Reply to Shaked Levy']"
+replyBox = driver.find_element(By.XPATH, box)
+emoj = driver.find_elements(By.XPATH,"//*[@aria-label='Insert an emoji']")
+emoj[1].click()
+sleep(1)
+animal = driver.find_element(By.XPATH, "//*[@aria-label='Animals & Nature']")
+animal.click()
+sleep(1)
+meat = driver.find_element(By.XPATH,
+                                "//*[@src='https://static.xx.fbcdn.net/images/emoji.php/v9/tb5/1/28/1f969.png']")
+meat.click()
+replyBox.send_keys(Keys.ENTER)
+
+
+while True:
+    time = datetime.datetime.now()
+    if 7 <= time.hour < 16 and 18 <= time.hour < 20:
+        nextTime = random.randint(60 * 5, 60 * 7)
+        sleep(nextTime)
+        try:
+            box = "//*[@aria-label='Reply to Shaked Levy']"
+            replyBox = driver.find_element(By.XPATH, box)
+            emoj = driver.find_elements(By.XPATH, "//*[@aria-label='Insert an emoji']")
+            emoj[1].click()
+            sleep(1)
+            animal = driver.find_element(By.XPATH, "//*[@aria-label='Animals & Nature']")
+            animal.click()
+            sleep(1)
+            meat = driver.find_element(By.XPATH,
+                                       "//*[@src='https://static.xx.fbcdn.net/images/emoji.php/v9/tb5/1/28/1f969.png']")
+            meat.click()
+            replyBox.send_keys(Keys.ENTER)
+        except Exception as e:
+            print("[ERROR]" + e)
