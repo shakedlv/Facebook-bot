28b7ffc1b7ff42a8dcf67c565c2d56a91bfa93b4
Shaked Levy <88069759+shakedlv@users.noreply.github.com>
2022-01-08 21:44:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -16,25 +16,10 @@ driver.get("https://www.facebook.com/189917291362525/posts/1608121936208713/")
 
 _input = input("Press enter after replay box clicked")
 
-box = "//*[@aria-label='Reply to Shaked Levy']"
-replyBox = driver.find_element(By.XPATH, box)
-emoj = driver.find_elements(By.XPATH,"//*[@aria-label='Insert an emoji']")
-emoj[1].click()
-sleep(1)
-animal = driver.find_element(By.XPATH, "//*[@aria-label='Animals & Nature']")
-animal.click()
-sleep(1)
-meat = driver.find_element(By.XPATH,
-                                "//*[@src='https://static.xx.fbcdn.net/images/emoji.php/v9/tb5/1/28/1f969.png']")
-meat.click()
-replyBox.send_keys(Keys.ENTER)
-
 
 while True:
     time = datetime.datetime.now()
-    if 7 <= time.hour < 16 and 18 <= time.hour < 20:
-        nextTime = random.randint(60 * 5, 60 * 7)
-        sleep(nextTime)
+    if 7 <= time.hour < 12 or 13 <= time.hour < 17 or 18 <= time.hour < 20:
         try:
             box = "//*[@aria-label='Reply to Shaked Levy']"
             replyBox = driver.find_element(By.XPATH, box)
@@ -48,5 +33,8 @@ while True:
                                        "//*[@src='https://static.xx.fbcdn.net/images/emoji.php/v9/tb5/1/28/1f969.png']")
             meat.click()
             replyBox.send_keys(Keys.ENTER)
+
         except Exception as e:
             print("[ERROR]" + e)
+        nextTime = random.randint(60 * 2, 60 * 5)
+        sleep(nextTime)
